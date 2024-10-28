de207e2821abe09754184c1a2c5ffd9e4a771283
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-10-28 16:13:19 +0330

    feat(chat): added loader for bot response

--- src/app/components/chat/ChatContainer.tsx
+++ src/app/components/chat/ChatContainer.tsx
@@ -6,7 +6,8 @@ import { MdSend } from "react-icons/md"
 import { useEffect, useRef, useState } from "react"
 import api from "@/external/api"
 import { BiMicrophone } from "react-icons/bi"
-import toast from "react-hot-toast"
+import toast, { Toaster } from "react-hot-toast"
+import axios from "axios"
 
 interface IProps {
    messages?: Message[]
@@ -32,30 +33,45 @@ const ChatContainer: React.FC<IProps> = ({
          role: "user",
       })
 
-      scrollToBottom()
-      const { data, status } = await api.post(
-         `/chats/${selectedChat?.slug}/messages`,
-         {
-            text,
-         }
-      )
-
-      if (status >= 400 && status < 600) {
-         toast.error(data.message)
-         setWaiting(false)
-         return
-      }
-
       messages?.push({
-         text: data.bot.text,
          role: "bot",
+         text: "waiting",
       })
 
-      setWaiting(false)
-
-      setText("")
-
       scrollToBottom()
+
+      try {
+         const { data } = await api.post(
+            `/chats/${selectedChat?.slug}/messages`,
+            {
+               text,
+            }
+         )
+
+         const waitingIndex = messages?.findLastIndex(
+            (message) => message.role == "bot"
+         )
+
+         if (waitingIndex && messages) {
+            messages[waitingIndex].text = data.bot.text
+         }
+
+         setWaiting(false)
+
+         setText("")
+
+         scrollToBottom()
+      } catch (error) {
+         setText("")
+         setWaiting(false)
+         messages?.pop()
+
+         if (axios.isAxiosError(error)) {
+            toast.error(error.response?.data.message)
+         } else {
+            toast.error("خطای نامشخصی رخ داده است")
+         }
+      }
    }
 
    const scrollToBottom = () => {
@@ -69,17 +85,18 @@ const ChatContainer: React.FC<IProps> = ({
    }, [messages])
 
    return (
-      <div className="flex flex-col relative min-h-screen bg-primary-300 pt-5 pb-20 overflow-auto">
-         {isLoading && (
-            <div className="flex justify-center items-center">
-               <Spinner color="primary" />
-            </div>
-         )}
-         <div className="flex flex-col gap-10 w-full px-4 md:px-7 lg:px-10">
-            {messages?.map((message, index) => (
-               <div
-                  key={index}
-                  className={`
+      <>
+         <div className="flex flex-col relative min-h-screen bg-primary-300 pt-5 pb-20 overflow-auto">
+            {isLoading && (
+               <div className="flex justify-center items-center">
+                  <Spinner color="primary" />
+               </div>
+            )}
+            <div className="flex flex-col gap-10 w-full px-4 md:px-7 lg:px-10">
+               {messages?.map((message, index) => (
+                  <div
+                     key={index}
+                     className={`
                      transition-all
                      flex
                      w-full
@@ -88,83 +105,99 @@ const ChatContainer: React.FC<IProps> = ({
                      justify-start
                      ${message.role == "user" ? "" : " flex-row-reverse"}
                   `}
-               >
-                  <Avatar
-                     name={message.role == "bot" ? "ربات" : "من"}
-                     src={message.role == "bot" ? "/support.png" : "/qmark.png"}
-                     className="shadow-3xl"
-                  />
-                  <div
-                     className={`
-                        max-w-[80%]
-                        md:max-w-[45%]
-                        min-h-[60px]
-                        p-5
-                        rounded-2xl
-                        shadow-xl
-                        ${
-                           message.role == "user"
-                              ? "bg-danger-100"
-                              : "bg-secondary-100"
-                        }
-                     `}
                   >
-                     {message.text}
+                     <Avatar
+                        name={message.role == "bot" ? "ربات" : "من"}
+                        src={
+                           message.role == "bot" ? "/support.png" : "/qmark.png"
+                        }
+                        className="shadow-3xl"
+                     />
+                     <div
+                        className={`
+                           max-w-[80%]
+                           md:max-w-[45%]
+                           min-h-[60px]
+                           p-5
+                           rounded-2xl
+                           shadow-xl
+                           ${
+                              message.role == "user"
+                                 ? "bg-danger-100"
+                                 : "bg-secondary-100"
+                           }
+                           transition-all
+                        `}
+                     >
+                        {message.text === "waiting" ? (
+                           <div className="flex items-center justify-center">
+                              <Spinner color="primary" />
+                           </div>
+                        ) : (
+                           message.text
+                        )}
+                     </div>
                   </div>
-               </div>
-            ))}
-         </div>
-         <div ref={endOfMessageRef} />
+               ))}
+            </div>
+            <div ref={endOfMessageRef} />
 
-         <div className="fixed bottom-0 flex justify-center items-center w-full bg-transparent">
-            <Textarea
-               radius="none"
-               minRows={1}
-               classNames={{
-                  innerWrapper: "items-center p-1",
-               }}
-               startContent={
-                  <>
-                     {text != "" ? (
-                        <Button
-                           isIconOnly
-                           variant="shadow"
-                           color="primary"
-                           onClick={sendToChat}
-                           radius="full"
-                           className="cursor-pointer"
-                           isLoading={waiting}
-                        >
-                           <MdSend size={22} />
-                        </Button>
-                     ) : (
-                        <Button
-                           isIconOnly
-                           variant="shadow"
-                           color="primary"
-                           onClick={sendToChat}
-                           disabled={text === ""}
-                           radius="full"
-                           isLoading={waiting}
-                           className="cursor-pointer"
-                        >
-                           <BiMicrophone size={22} />
-                        </Button>
-                     )}
-                  </>
-               }
-               placeholder="سوال خود را اینجا بنویسید..."
-               onValueChange={setText}
-               value={text}
-               onKeyDown={(e) => {
-                  if ((e.ctrlKey || e.metaKey) && e.key == "Enter") {
-                     e.preventDefault()
-                     sendToChat()
+            <div className="fixed bottom-0 flex justify-center items-center w-full bg-transparent">
+               <Textarea
+                  radius="none"
+                  minRows={1}
+                  classNames={{
+                     innerWrapper: "items-center p-1",
+                  }}
+                  startContent={
+                     <>
+                        {text != "" ? (
+                           <Button
+                              isIconOnly
+                              variant="shadow"
+                              color="primary"
+                              onClick={sendToChat}
+                              radius="full"
+                              className="cursor-pointer"
+                              isLoading={waiting}
+                           >
+                              <MdSend size={22} />
+                           </Button>
+                        ) : (
+                           <Button
+                              isIconOnly
+                              variant="shadow"
+                              color="primary"
+                              onClick={sendToChat}
+                              disabled={text === ""}
+                              radius="full"
+                              isLoading={waiting}
+                              className="cursor-pointer"
+                           >
+                              <BiMicrophone size={22} />
+                           </Button>
+                        )}
+                     </>
                   }
-               }}
-            />
+                  placeholder="سوال خود را اینجا بنویسید..."
+                  onValueChange={setText}
+                  value={text}
+                  onKeyDown={(e) => {
+                     if ((e.ctrlKey || e.metaKey) && e.key == "Enter") {
+                        e.preventDefault()
+                        sendToChat()
+                     }
+                  }}
+               />
+            </div>
          </div>
-      </div>
+         <Toaster
+            position="top-center"
+            toastOptions={{
+               duration: 3000,
+            }}
+         />
+      </>
    )
 }
 
